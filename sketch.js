afd1f77baf7e8bf156f1a13c8d663a305e2ef4e7
Marco4814 <107722878+Marco4814@users.noreply.github.com>
2022-06-17 18:06:00 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var student
+
+function setup() {
+  createCanvas(400,400);
+  student = new Student("matheus", 16, 9)
+  student.display()
+}
+
+function draw() 
+{
+  background(30);
+  
+}
---
+++ student.js
@@ -0,0 +1,20 @@
+
+class Student
+{ constructor(nome, idade, serie){
+ 
+    this.nome = nome;
+    this.idade = idade;
+    this.serie = serie;
+
+}
+   display(){
+
+    console.log(this.nome)
+    console.log(this.idade)
+    console.log(this.serie)
+
+   }
+}
+
+
+
